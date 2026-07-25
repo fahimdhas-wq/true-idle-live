b2be5e1cf64e6221422cd122fac5756462c1e2c1
fahimdhas-wq <fahimdhas-wq@users.noreply.github.com>
2026-07-25 03:37:39 +0000

    Deploy True IDLE build c3c39997624ff0be86c1dd5382585385a9d20124 fahimdhas-wq/true-idle@c3c39997624ff0be86c1dd5382585385a9d20124

---
+++ assets/OfflineFarming-jNT5hZnR.js
@@ -0,0 +1,1 @@
+class t{simulate(a,s){return{systemId:"farming",xpGained:{},itemsGained:{},goldGained:0,killCount:0,mobs:{},metadata:{}}}}export{t as OfflineFarming};
---
+++ assets/OfflineMining-1IM-RDaf.js
@@ -0,0 +1,1 @@
+class s{simulate(i,n){return{systemId:"mining",xpGained:{},itemsGained:{},goldGained:0,killCount:0,mobs:{},metadata:{}}}}export{s as OfflineMining};
